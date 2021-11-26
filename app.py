6dd4462ac301400b596d7eaeb608b74a69158a80
Gino Tria <gino.tr14@gmail.com>
2021-11-26 10:06:58 +0800

    added function to add movies

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import database
+import datetime
 
 menu = """Please select one of the following options:
 1) Add new movie.
@@ -15,9 +16,20 @@ welcome = "Welcome to the watchlist app!"
 print(welcome)
 database.create_tables()
 
-while (user_input := input(menu)) != "6":
+
+def prompt_add_movie():
+    title = input("Movie title: ")
+    release_date = input("Release date (mm-dd-YYYY): ")
+    # string -> date -> timestamp
+    parsed_date = datetime.datetime.strptime(release_date, "%m-%d-%Y")
+    timestamp = parsed_date.timestamp()
+    database.add_movie(title, timestamp)
+
+
+user_input = input(menu)
+while (user_input) != "6":
     if user_input == "1":
-        pass
+        prompt_add_movie()
     elif user_input == "2":
         pass
     elif user_input == "3":
@@ -28,3 +40,5 @@ while (user_input := input(menu)) != "6":
         pass
     else:
         print("Invalid input, please try again!")
+
+    user_input = input(menu)
