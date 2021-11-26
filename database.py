eba00b184b91fa55ad26562bd8460ea1d8455342
Gino Tria <gino.tr14@gmail.com>
2021-11-26 09:56:05 +0800

    added databased methods

--- database.py
+++ database.py
@@ -1,10 +1,5 @@
-# Queries
-# Create the table *
-# Insert a movie *
-# Select all movies *
-# Select upcoming movies *
-# Select watched movies *
-# Set a movie to “watched” ???
+import datetime
+import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
   title TEXT,
@@ -15,3 +10,43 @@ INSERT_MOVIE = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+
+connection = sqlite3.connect("data.db")
+
+
+def create_tables():
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+
+def add_movie(title, release_timestamp):
+    with connection:
+        connection.execute(INSERT_MOVIE, (title, release_timestamp))
+
+
+def get_all_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+
+def get_upcoming_movies():
+    with connection:
+        cursor = connection.cursor()
+        today_timestamp = datetime.datetime.today().timestamp()
+        cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        return cursor.fetchall()
+
+
+def get_watched_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
+
+
+def watch_movie(movie_title):
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (movie_title,))
