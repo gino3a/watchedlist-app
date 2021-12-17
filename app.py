02a88abaa788fc324c54f5ce884e3f603b368805
Gino Tria <gino.tr14@gmail.com>
2021-12-17 08:55:09 +0800

    Stage 2 updates

--- app.py
+++ app.py
@@ -37,9 +37,15 @@ def display_movie_list(title, movies):
 
 
 def prompt_watch_movie():
+    username = input("Username: ")
     movie_title = input("Enter movie title you've watched: ")
-    database.watch_movie(movie_title)
+    database.watch_movie(username, movie_title)
 
+def print_watched_movie_list(username, movies):
+    print(f"-- {username}'s watched movies --")
+    for movie in movies:
+        print(f"{movie[1]}")
+    print("---- \n")
 
 user_input = input(menu)
 while (user_input) != "6":
@@ -54,8 +60,9 @@ while (user_input) != "6":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        watched_movies = database.get_watched_movies()
-        display_movie_list('Watched Movies', watched_movies)
+        username = input("Username: ")
+        movies = database.get_watched_movies(username)
+        print_watched_movie_list(username, movies)
     else:
         print("Invalid input, please try again!")
 
--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
   title TEXT,
-  release_timestamp REAL,
+  release_timestamp REAL
 );"""
 
 CREATE_WATCHLIST_TABLE = """CREATE TABLE IF NOT EXISTS watched (
@@ -26,6 +26,7 @@ connection = sqlite3.connect("data.db")
 def create_tables():
     with connection:
         connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_WATCHLIST_TABLE)
 
 
 def add_movie(title, release_timestamp):
@@ -48,13 +49,14 @@ def get_upcoming_movies():
         return cursor.fetchall()
 
 
-def get_watched_movies():
+def get_watched_movies(username):
     with connection:
         cursor = connection.cursor()
-        cursor.execute(SELECT_WATCHED_MOVIES)
+        cursor.execute(SELECT_WATCHED_MOVIES, (username,))
         return cursor.fetchall()
 
 
-def watch_movie(movie_title):
+def watch_movie(username, movie_title):
     with connection:
-        connection.execute(SET_MOVIE_WATCHED, (movie_title,))
+        connection.execute(DELETE_MOVIE, (movie_title,))
+        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_title))
